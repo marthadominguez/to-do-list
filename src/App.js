418a70fdc8e5c14cc3aeda4e0503300e0d6aeaaa
Martha Dominguez <marthadominguezg@gmail.com>
2022-01-25 12:45:56 -0500

    actualizar lista con todos completados

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { TodoItem } from './components/TodoItem';
 import { TodoList } from './components/TodoList';
 
 function App() {
+
   const todosDefault = [
     { text: "Mercar", completed: false },
     { text: "Comprar verduras", completed: true },
@@ -11,8 +12,8 @@ function App() {
   ]
 
   const [busqueda, setBusqueda] = useState("");
-  const [ todos, setTodos ] = useState(todosDefault)
-  const todosCompletados = todos.filter(todo => todo.completed ).length;
+  const [todos, setTodos] = useState(todosDefault)
+  const todosCompletados = todos.filter(todo => todo.completed).length;
   const todosTotal = todos.length;
 
   let todosBuscados = [];
@@ -26,22 +27,42 @@ function App() {
     });
   }
 
-  return (
-    <div className="App flex justify-center h-screen items-center">
-      <div className="bg-indigo-400 p-12 flex flex-col">
-        <h2 className="text-3xl">Crea una nueva tarea</h2>
-        <span className="">Nombre de la tarea:</span>
-        <input className="" placeholder="Conquistar el mundo..."></input>
-        <button type="button" onClick={ console.log() } >Crear tarea</button>
-      </div>
-      <div className="bg-emerald-500 p-12 flex flex-col" >
-        <h2 className="text-3xl" >Tus tareas </h2>
-        <span>Has completado {todosCompletados} de {todosTotal} tareas.</span>
-        <TodoSearch busqueda={busqueda} setBusqueda={setBusqueda} ></TodoSearch>
-        <TodoList>{todosBuscados.map((todo, index) => <TodoItem key={index} text={todo.text}></TodoItem>)}</TodoList>
-      </div>
+  console.log(todos)
+
+  const completarTodos = (texto) => {
+    const indexTodo = todos.findIndex(todo => todo.text === texto);
+    const newTodos = [...todos];
+    newTodos[indexTodo] = {
+      text: todos[indexTodo].text,
+      completed: true
+    };
+    setTodos(newTodos);
+  }
+
+return (
+  <div className="App flex justify-center h-screen items-center">
+    <div className="bg-lime-200 p-12 flex flex-col w-1/3 h-96">
+      <h2 className="text-3xl">Crea una nueva tarea</h2>
+      <span className="my-4">Nombre de la tarea:</span>
+      <input className="my-4" placeholder="Conquistar el mundo..."></input>
+      <button className="my-4" type="button" onClick={console.log()} >Crear tarea</button>
     </div>
-  );
+    <div className="bg-emerald-200 p-12 flex flex-col w-1/3 h-96" >
+      <h2 className="text-3xl" >Tus tareas </h2>
+      <span className="my-4" >Has completado {todosCompletados} de {todosTotal} tareas.</span>
+      <TodoSearch busqueda={busqueda} setBusqueda={setBusqueda} ></TodoSearch>
+      <TodoList>
+        {todosBuscados.map((todo, index) => (
+          <TodoItem
+            key={index}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completarTodos(todo.text) }>
+          </TodoItem>))}
+      </TodoList>
+    </div>
+  </div>
+);
 }
 
 export default App;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const TodoItem = ({text}) => {
+export const TodoItem = ({text, onComplete}) => {
   return (
-    <div>
-      <span>{text}</span>
-      <input type="text" />
-      <button>+</button>
+    <div className="my-4">
+      <button className="mx-4" onClick={onComplete}>check</button>
+      <span >{text}</span>
+      <button className="mx-4" >delete</button>
     </div>
   );
 };
