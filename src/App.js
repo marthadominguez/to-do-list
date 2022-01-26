3d9e3ac62808eda747d48f0af2af5115f02c548f
Martha Dominguez <marthadominguezg@gmail.com>
2022-01-25 23:25:26 -0500

    persistencia de datos en el localStorage

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import { TodoList } from './components/TodoList';
 
 function App() {
 
-  const todosDefault = [
-    { text: "Mercar", completed: false },
-    { text: "Comprar verduras", completed: true },
-    { text: "Hacer aseo", completed: true }
-  ]
+  const localStorageTodos = localStorage.getItem('TODOS_V1')
+  let parsedTodos;
+  if (!localStorageTodos) {
+    // no existen todos todavía, le asignamos entonces una lista vacía.
+    localStorage.setItem('TODOS_V1', JSON.stringify([]))
+    parsedTodos = []
+  } else {
+    //todos ya existentes
+    parsedTodos = JSON.parse(localStorageTodos)
+  }
 
   const [busqueda, setBusqueda] = useState("");
-  const [todos, setTodos] = useState(todosDefault)
+  const [todos, setTodos] = useState(parsedTodos)
   const todosCompletados = todos.filter(todo => todo.completed).length;
   const todosTotal = todos.length;
 
@@ -27,7 +32,11 @@ function App() {
     });
   }
 
-  console.log(todos)
+  const guardarTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  }
 
   const completarTodos = (texto) => {
     const indexTodo = todos.findIndex(todo => todo.text === texto);
@@ -36,33 +45,42 @@ function App() {
       text: todos[indexTodo].text,
       completed: true
     };
-    setTodos(newTodos);
+    guardarTodos(newTodos);
   }
 
-return (
-  <div className="App flex justify-center h-screen items-center">
-    <div className="bg-lime-200 p-12 flex flex-col w-1/3 h-96">
-      <h2 className="text-3xl">Crea una nueva tarea</h2>
-      <span className="my-4">Nombre de la tarea:</span>
-      <input className="my-4" placeholder="Conquistar el mundo..."></input>
-      <button className="my-4" type="button" onClick={console.log()} >Crear tarea</button>
+  const eliminarTodos = (texto) => {
+    const indexTodo = todos.findIndex(todo => todo.text === texto);
+    const newTodos = [...todos];
+    newTodos.splice(indexTodo, 1);
+    guardarTodos(newTodos);
+  }
+
+  return (
+    <div className="App flex justify-center h-screen items-center">
+      <div className="bg-lime-200 p-12 flex flex-col w-1/3 h-96">
+        <h2 className="text-3xl">Crea una nueva tarea</h2>
+        <span className="my-4">Nombre de la tarea:</span>
+        <input className="my-4" placeholder="Conquistar el mundo..."></input>
+        <button className="my-4" type="button" onClick={console.log()} >Crear tarea</button>
+      </div>
+      <div className="bg-emerald-200 p-12 flex flex-col w-1/3 h-96" >
+        <h2 className="text-3xl" >Tus tareas </h2>
+        <span className="my-4" >Has completado {todosCompletados} de {todosTotal} tareas.</span>
+        <TodoSearch busqueda={busqueda} setBusqueda={setBusqueda} ></TodoSearch>
+        <TodoList>
+          {todosBuscados.map((todo, index) => (
+            <TodoItem
+              key={index}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completarTodos(todo.text)}
+              onDelete={()=> eliminarTodos(todo.text)}
+              >
+            </TodoItem>))}
+        </TodoList>
+      </div>
     </div>
-    <div className="bg-emerald-200 p-12 flex flex-col w-1/3 h-96" >
-      <h2 className="text-3xl" >Tus tareas </h2>
-      <span className="my-4" >Has completado {todosCompletados} de {todosTotal} tareas.</span>
-      <TodoSearch busqueda={busqueda} setBusqueda={setBusqueda} ></TodoSearch>
-      <TodoList>
-        {todosBuscados.map((todo, index) => (
-          <TodoItem
-            key={index}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completarTodos(todo.text) }>
-          </TodoItem>))}
-      </TodoList>
-    </div>
-  </div>
-);
+  );
 }
 
 export default App;
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const TodoItem = ({text, onComplete}) => {
+export const TodoItem = ({text, onComplete, onDelete}) => {
   return (
     <div className="my-4">
       <button className="mx-4" onClick={onComplete}>check</button>
       <span >{text}</span>
-      <button className="mx-4" >delete</button>
+      <button className="mx-4" onClick={onDelete}>delete</button>
     </div>
   );
 };
